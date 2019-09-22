70140cfbe8114fe36258be1193b53880a7d5e14c
Mo Sha <desert0616@gmail.com>
2019-09-23 00:29:36 +0800

    revise

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,9 +51,4 @@ jQuery(document).ready(function($) {
         
         }
     );
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
-
-
 });
